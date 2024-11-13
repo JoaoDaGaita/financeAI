8756f6138d36e02030409a81d2bb69b7f6fbf75d
joaodagaita <joaovsilva70@outlook.com>
2024-11-13 00:28:18 -0300

    feat: add stripe webhook that handles subscriptions

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import Navbar from '../_components/navbar'
-import { auth } from '@clerk/nextjs/server'
+import { auth, clerkClient } from '@clerk/nextjs/server'
 import { redirect } from 'next/navigation'
 import { Card, CardContent, CardHeader } from '../_components/ui/card'
 import { CheckIcon, XIcon } from 'lucide-react'
 import AcquirePlanButton from './_components/acquire-plan-button'
+import { Badge } from '../_components/ui/badge'
 
 const SubscritpionPage = async () => {
   const { userId } = await auth()
   if (!userId) {
     redirect('/login')
   }
+  const user = await clerkClient().users.getUser(userId)
+  const hasPremiumPlan = user?.publicMetadata.subscriptionPlan == 'premium'
 
   return (
     <>
@@ -27,7 +30,7 @@ const SubscritpionPage = async () => {
               <div className="flex items-center justify-center gap-3">
                 <span className="text-4xl">R$</span>
                 <span className="text-6xl">0</span>
-                <span className="text-2xl text-muted-foreground">mês</span>
+                <span className="text-2xl text-muted-foreground">/mês</span>
               </div>
             </CardHeader>
             <CardContent className="space-y-6 py-8">
@@ -43,14 +46,19 @@ const SubscritpionPage = async () => {
           </Card>
 
           <Card className="w-[450px]">
-            <CardHeader className="border-b border-solid py-8">
+            <CardHeader className="relative border-b border-solid py-8">
+              {hasPremiumPlan && (
+                <Badge className="absolute left-4 top-12 bg-primary/10 text-primary">
+                  Ativo
+                </Badge>
+              )}
               <h2 className="text-center text-2xl font-semibold">
-                Plano Básico
+                Plano Premium
               </h2>
               <div className="flex items-center justify-center gap-3">
                 <span className="text-4xl">R$</span>
-                <span className="text-6xl">0</span>
-                <span className="text-2xl text-muted-foreground">mês</span>
+                <span className="text-6xl">19</span>
+                <span className="text-2xl text-muted-foreground">/mês</span>
               </div>
             </CardHeader>
             <CardContent className="space-y-6 py-8">
