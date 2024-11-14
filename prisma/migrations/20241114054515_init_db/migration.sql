3e8b54ca1a8b1c546a68d0b8f0383d2e835d6ebe
joaodagaita <joaovsilva70@outlook.com>
2024-11-14 02:46:31 -0300

    fix: migrations erros

--- prisma/migrations/20241114054515_init_db/migration.sql
+++ prisma/migrations/20241114054515_init_db/migration.sql
@@ -1,11 +1,11 @@
 -- CreateEnum
-CREATE TYPE "TransactionPaymentMethod" AS ENUM ('CREDIT_CARD', 'DEBIT_CARD', 'BANK_TRANSFER', 'BANK_SLIP', 'CASH', 'PIX', 'OTHER');
-
--- CreateEnum
 CREATE TYPE "TransactionType" AS ENUM ('DEPOSIT', 'EXPENSE', 'INVESTMENT');
 
 -- CreateEnum
 CREATE TYPE "TransactionCategory" AS ENUM ('HOUSING', 'TRANSPORTATION', 'FOOD', 'ENTERTAINMENT', 'HEALTH', 'UTILITY', 'SALARY', 'EDUCATION', 'OTHER');
+
+-- CreateEnum
+CREATE TYPE "TransactionPaymentMethod" AS ENUM ('CREDIT_CARD', 'DEBIT_CARD', 'BANK_TRANSFER', 'BANK_SLIP', 'CASH', 'PIX', 'OTHER');
 
 -- CreateTable
 CREATE TABLE "Transaction" (
@@ -13,9 +13,9 @@ CREATE TABLE "Transaction" (
     "name" TEXT NOT NULL,
     "type" "TransactionType" NOT NULL,
     "amount" DECIMAL(10,2) NOT NULL,
-    "date" TIMESTAMP(3) NOT NULL,
     "category" "TransactionCategory" NOT NULL,
     "paymentMethod" "TransactionPaymentMethod" NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
     "userId" TEXT NOT NULL,
